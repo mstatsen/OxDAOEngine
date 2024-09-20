153c28592816aa3e491ab8fdc18f3953048ec530
AOxide <aoxide76@ya.ru>
2024-09-20 12:01:48 +0300

    fix horizontal linkButtons sizes

--- ControlFactory/Controls/Links/LinkButtonList.cs
+++ ControlFactory/Controls/Links/LinkButtonList.cs
@@ -1,15 +1,35 @@
-﻿using OxLibrary.Panels;
+﻿using OxLibrary;
+using OxLibrary.Panels;
 
 namespace OxDAOEngine.ControlFactory.Controls
 {
     public class LinkButtonList : OxPanel
     {
-        public ButtonListDirection Direction = ButtonListDirection.Vertical;
+        private ButtonListDirection direction = ButtonListDirection.Vertical;
+        public ButtonListDirection Direction
+        {
+            get => direction;
+            set
+            {
+                direction = value;
+
+                switch (direction)
+                {
+                    case ButtonListDirection.Vertical:
+                        Dock = DockStyle.None;
+                        break;
+                    case ButtonListDirection.Horizontal:
+                        Dock = DockStyle.Top;
+                        Paddings.TopOx = OxSize.Extra;
+                        break;
+                }
+            }
+        }
 
         public LinkButtonList() : base() { }
 
         public readonly List<LinkButton> Buttons = new();
-        private const int ButtonSpace = 3;
+        private const int ButtonSpace = 2;
         private const int ButtonHeight = 22;
 
         private int LastBottom =>
@@ -27,6 +47,15 @@ namespace OxDAOEngine.ControlFactory.Controls
             Direction == ButtonListDirection.Horizontal
                 ? 0
                 : LastBottom + ButtonSpace;
+
+        private void RecalcButtonsPositions()
+        {
+            if (Direction == ButtonListDirection.Horizontal)
+            {
+                foreach (LinkButton button in Buttons)
+                    button.Left = Buttons.IndexOf(button) * (button.Width + ButtonSpace);
+            }
+        }
 
         private void RecalcButtonsSize()
         {
@@ -49,27 +78,41 @@ namespace OxDAOEngine.ControlFactory.Controls
             button.Parent = this;
             button.Left = ButtonLeft();
             button.Top = ButtonTop();
-            SetButtonSize(button);
             Buttons.Add(button);
+            SetButtonsSize();
             RecalcButtonsSize();
+            RecalcButtonsPositions();
         }
 
         protected override void OnSizeChanged(EventArgs e)
         {
             base.OnSizeChanged(e);
+            SetButtonsSize();
+            RecalcButtonsPositions();
+        }
 
+        private void SetButtonsSize()
+        {
             foreach (LinkButton button in Buttons)
                 SetButtonSize(button);
         }
 
-        private void SetButtonSize(LinkButton button) =>
+        private void SetButtonSize(LinkButton button)
+        {
+            int calcedWidth =
+                Direction == ButtonListDirection.Vertical
+                    ? Width - button.BorderWidth * 2
+                    : (Buttons.Count == 0
+                        ? 120
+                        : Math.Min(Width / Buttons.Count, 120)
+                            - ButtonSpace * (Buttons.Count - 1))
+                    - button.BorderWidth * 2;
+
             button.SetContentSize(
-                (Direction == ButtonListDirection.Vertical
-                    ? Width
-                    : 120)
-                    - button.BorderWidth * 2,
+                calcedWidth,
                 ButtonHeight
             );
+        }
 
         public void Clear()
         {
@@ -83,7 +126,14 @@ namespace OxDAOEngine.ControlFactory.Controls
         private void RecalcHeight() =>
             MinimumSize = new Size(0, LastBottom + 2);
 
-        private void RecalcWidth() =>
-            MinimumSize = new Size(LastRight, ButtonHeight);
+        private void RecalcWidth()
+        {
+            MinimumSize = new Size(
+                OxDockHelper.IsVertical(OxDockHelper.Dock(Dock)) 
+                    ? Width 
+                    : LastRight, 
+                40
+            );
+        }
     }
 }
