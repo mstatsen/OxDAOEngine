1f8cdf4b2f3f45517036b11041cf1a68ca02ba13
AOxide <aoxide76@ya.ru>
2024-10-24 11:19:21 +0300

    account html decorator

--- Data/Decorator/DecoratorHelper.cs
+++ Data/Decorator/DecoratorHelper.cs
@@ -2,6 +2,22 @@
 {
     public static class DecoratorHelper
     {
+        public static string ListToString<TDAO>(ListDAO<TDAO> list, string separator, bool noWrapItems = true)
+            where TDAO : DAO, new() =>
+            ListToString(list.List, separator, noWrapItems);
+
+        public static string ListToString<T>(List<T> list, string separator, bool noWrapItems = true)
+        { 
+            List<string?> stringList = new();
+
+            foreach (T item in list)
+                if (item != null)
+                    stringList.Add(item.ToString());
+
+            return ListToString(stringList, separator, noWrapItems);
+
+        }
+
         public static string ListToString(List<string?> list, string separator, bool noWrapItems = true)
         {
             string result = string.Empty;
